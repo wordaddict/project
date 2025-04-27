ce483d95bd0b4ad586ff8f1b42ba9fabafa7e999
Micheal Adeyinka <michealadeyinka@micheals-mbp.myfiosgateway.com>
2025-04-27 19:48:12 -0400

    add redirect

--- backend/main.py
+++ backend/main.py
@@ -15,16 +15,12 @@ app = FastAPI()
 
 # Allow requests from your frontend domain
 origins = [
-    "https://wordaddict.github.io/project",  # Your Render frontend URL
-    "http://localhost:8000",                       
-    "http://127.0.0.1:8000",
-    "http://localhost:5500",                       
-    "http://127.0.0.1:5500"                               
+    "https://wordaddict.github.io",  # Your Render frontend URL                           
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
